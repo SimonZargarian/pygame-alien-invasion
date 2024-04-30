949ea032f41a786e99d15dc20aed634004ae382a
Simon Zargarian <simonzargariancv@gmail.com>
2024-04-30 15:10:46 +0200

    added scoreboard function

--- src/alien_invasion-package_simon_zargarian_cv/alien_invasion.py
+++ src/alien_invasion-package_simon_zargarian_cv/alien_invasion.py
@@ -4,6 +4,8 @@ from settings import Settings
 from ship import Ship
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 import game_funtions as gf
 
 def run_game():
@@ -13,6 +15,9 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
 
     # Make a ship
     ship = Ship(ai_settings, screen)
@@ -24,19 +29,21 @@ def run_game():
     # Create the fleet of aliens
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
-    # Create an instance to store game statistics
+    # Create an instance to store game statistics and create a scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # Start the amin loop for the game.
     while True:
 
         # Watch for keyboard and mouse events.
-        gf.check_events(ai_settings,screen, ship, bullets)
+        gf.check_events(ai_settings,screen, stats, sb, play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
         # print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+                         bullets, play_button)
 
 run_game()
--- src/alien_invasion-package_simon_zargarian_cv/game_stats.py
+++ src/alien_invasion-package_simon_zargarian_cv/game_stats.py
@@ -8,6 +8,13 @@ class GameStats():
         # Start Alien Invasion in an active state.
         self.game_active = True
 
+        # Start game in an inactive state.
+        self.game_active = False
+
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
